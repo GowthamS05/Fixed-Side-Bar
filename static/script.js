d5d04778ca4bb4ba395097651434e881af9447b5
unknown <raogowtham@gmail.com>
2017-11-04 18:03:55 +0530

    1st commit

---
+++ static/script.js
@@ -0,0 +1,64 @@
+var width = 'customWidth';
+
+function increaseWidth() {
+    if (width == 'customWidth') {
+        document.getElementById("content-div").classList.add('set-fullWidthDiv');
+        document.getElementById("content-div").classList.remove('set-calculatedWidthDiv');
+        width = 'autoWidth';
+
+    } else if (width == 'autoWidth') {
+        document.getElementById("content-div").classList.add('set-calculatedWidthDiv');
+        document.getElementById("content-div").classList.remove('set-fullWidthDiv');
+        width = 'customWidth';
+
+    }
+}
+jQuery(function($) {
+    var $bodyEl = $('body'),
+        $sidedrawerEl = $('#sidedrawer');
+
+
+    // ==========================================================================
+    // Toggle Sidedrawer
+    // ==========================================================================
+    function showSidedrawer() {
+        // show overlay
+        var options = {
+            onclose: function() {
+                $sidedrawerEl
+                    .removeClass('active')
+                    .appendTo(document.body);
+            }
+        };
+
+        var $overlayEl = $(mui.overlay('on', options));
+
+        // show element
+        $sidedrawerEl.appendTo($overlayEl);
+        setTimeout(function() {
+            $sidedrawerEl.addClass('active');
+        }, 20);
+    }
+
+
+    function hideSidedrawer() {
+        $bodyEl.toggleClass('hide-sidedrawer');
+    }
+
+
+    $('.js-show-sidedrawer').on('click', showSidedrawer);
+    $('.js-hide-sidedrawer').on('click', hideSidedrawer);
+
+    // ==========================================================================
+    // Animate menu
+    // ==========================================================================
+    var $titleEls = $('strong', $sidedrawerEl);
+
+    $titleEls
+        .next()
+        .hide();
+
+    $titleEls.on('click', function() {
+        $(this).next().slideToggle(200);
+    });
+});
